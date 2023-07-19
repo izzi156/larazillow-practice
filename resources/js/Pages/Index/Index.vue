07558449e163bc88e37b68482627337b04cf7682
izzi156 <izziainiwaizah@gmail.com>
2023-07-20 02:04:18 +0800

    First Commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+    <div>
+        The message is {{ message }}
+    </div>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue';
+    defineProps({
+        message: String
+    })
+    
+</script>
+
